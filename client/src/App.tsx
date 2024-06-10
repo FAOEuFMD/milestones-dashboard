ae737143285e6c155a21be2913fe4a123510f634
arixnapa <arianne.napa@gmail.com>
2024-06-10 14:44:57 +0200

    feat(page): create Dashboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Page404 from "./pages/Page404";
+import Dashboard from "./pages/Dashboard";
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
