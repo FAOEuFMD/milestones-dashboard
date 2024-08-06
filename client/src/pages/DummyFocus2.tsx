bf66d2c5ea3e0b31de5062d344785e3b0cab823a
Andrea Cortés <132715367+ACU017@users.noreply.github.com>
2024-08-06 20:00:40 +0200

    Fix/navbar logo (#31)
    * Fixed the navbarlogo to take to home and the routes for the buttons
    
    * Fixed plott background color and paper color

--- client/src/pages/DummyFocus2.tsx
+++ client/src/pages/DummyFocus2.tsx
@@ -12,6 +12,7 @@ const DummyFocus2: React.FC = () => {
           Dummy landing page2
         </p>
         <Plot
+          className="plot py-8 px-8"
           data={[
             {
               x: [1, 2, 3],
@@ -22,7 +23,15 @@ const DummyFocus2: React.FC = () => {
             },
             { type: "bar", x: [1, 2, 3], y: [2, 5, 3] },
           ]}
-          layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+          layout={{
+            width: 700,
+            height: 500,
+            title: "A Fancy Plot",
+            //This changes the color ih the graph background
+            plot_bgcolor: "#0000",
+            //This changes the color of the paper
+            paper_bgcolor: "#0000",
+          }}
         />
       </div>
     </div>
