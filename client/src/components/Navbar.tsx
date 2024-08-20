58d5b450da45ed0703a7cf0ff9437952bd1c3a64
Andrea <anfrescortes@gmail.com>
2024-08-20 20:41:43 +0200

    refactor : removed select and icons + fixed logo height

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faCalendarAlt,
-  faFlag,
-} from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
 import logo from "../assets/faologo.png";
 
 const Navbar: React.FC = () => {
@@ -21,35 +16,20 @@ const Navbar: React.FC = () => {
             <img
               src={logo}
               alt="FAO Logo"
-              className="h-8"
+              className="h-40"
+              // these are the setting from the on render logo
+              //class="w-16 md:w-28 h-auto my-2 md:my-4"
               onClick={takeToHome}
             />
-            <select className="bg-green-700 text-green-200 py-2 px-4 rounded">
-              <option>Select Dashboard</option>
-              <option>Dashboard 1</option>
-              <option>Dashboard 2</option>
-            </select>
           </div>
-          {/* search bar */}
+          {/* search bar
           <div className="flex-grow mx-4">
             <input
               type="text"
               placeholder="Search..."
               className="w-full py-2 px-4 rounded-full bg-green-700 text-green-200 border border-green-400 focus:outline-none focus:ring focus:border-green-500 placeholder-white"
             />
-          </div>
-          {/* icons */}
-          <div className="flex items-center space-x-4 text-green-200">
-            <Link to="/profile" className="hover:text-green-100">
-              <FontAwesomeIcon icon={faUser} size="lg" />
-            </Link>
-            <Link to="/calendar" className="hover:text-green-100">
-              <FontAwesomeIcon icon={faCalendarAlt} size="lg" />
-            </Link>
-            <Link to="/milestones" className="hover:text-green-100">
-              <FontAwesomeIcon icon={faFlag} size="lg" />
-            </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
