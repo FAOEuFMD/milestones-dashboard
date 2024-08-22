3e7095dc424081894cc6b1f66ab252e6ef735812
Andrea Cortés <132715367+ACU017@users.noreply.github.com>
2024-08-22 21:05:41 +0200

    refactor : plottly.js integration + removed select and icons + fixed logo height (#36)
    * refactor : removed select and icons + fixed logo height
    
    * Updated the assets with the new logo as the current one is deprecated
    
    * Updated the event handler and replaced with the Link from React router

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,55 +1,33 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faCalendarAlt,
-  faFlag,
-} from "@fortawesome/free-solid-svg-icons";
-import logo from "../assets/faologo.png";
+import { Link } from "react-router-dom";
+
+import logo from "../assets/EuFMD_2023_white_xl.png";
 
 const Navbar: React.FC = () => {
-  const navigate = useNavigate();
-  const takeToHome = () => navigate("/");
-
   return (
     <nav className="bg-green-600 shadow-lg">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
           {/* logo */}
           <div className="flex items-center space-x-4">
-            <img
-              src={logo}
-              alt="FAO Logo"
-              className="h-8"
-              onClick={takeToHome}
-            />
-            <select className="bg-green-700 text-green-200 py-2 px-4 rounded">
-              <option>Select Dashboard</option>
-              <option>Dashboard 1</option>
-              <option>Dashboard 2</option>
-            </select>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="FAO Logo"
+                className="h-60"
+                // these are the setting from the on render logo
+                //class="w-16 md:w-28 h-auto my-2 md:my-4"
+              />
+            </Link>
           </div>
-          {/* search bar */}
+          {/* search bar
           <div className="flex-grow mx-4">
             <input
               type="text"
               placeholder="Search..."
               className="w-full py-2 px-4 rounded-full bg-green-700 text-green-200 border border-green-400 focus:outline-none focus:ring focus:border-green-500 placeholder-white"
             />
-          </div>
-          {/* icons */}
-          <div className="flex items-center space-x-4 text-green-200">
-            <Link to="/profile" className="hover:text-green-100">
-              <FontAwesomeIcon icon={faUser} size="lg" />
-            </Link>
-            <Link to="/calendar" className="hover:text-green-100">
-              <FontAwesomeIcon icon={faCalendarAlt} size="lg" />
-            </Link>
-            <Link to="/milestones" className="hover:text-green-100">
-              <FontAwesomeIcon icon={faFlag} size="lg" />
-            </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
