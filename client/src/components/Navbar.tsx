b5e8b042f816f4ed6341aa4bb489058b19fbcd96
Irene Zatti <irene.zatti16@gmail.com>
2024-08-26 14:52:00 +0200

    fix: logo and navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,26 +8,15 @@ const Navbar: React.FC = () => {
     <nav className="bg-green-600 shadow-lg">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
-          {/* logo */}
           <div className="flex items-center space-x-4">
             <Link to="/">
               <img
                 src={logo}
                 alt="FAO Logo"
-                className="h-60"
-                // these are the setting from the on render logo
-                //class="w-16 md:w-28 h-auto my-2 md:my-4"
+                className="max-h-24 max-w-xs h-auto w-auto"
               />
             </Link>
           </div>
-          {/* search bar
-          <div className="flex-grow mx-4">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="w-full py-2 px-4 rounded-full bg-green-700 text-green-200 border border-green-400 focus:outline-none focus:ring focus:border-green-500 placeholder-white"
-            />
-          </div> */}
         </div>
       </div>
     </nav>
