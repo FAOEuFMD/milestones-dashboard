e9eb9328b8affc70e6f1aaed781f90e061a6f1aa
Sophie <sophie@sssoja.com>
2024-08-27 13:37:39 +0200

    styling: fix navbar margin

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,22 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import logo from "../assets/EuFMD_2023_white_xl.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-green-600 shadow-lg">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between items-center py-4">
-          <div className="flex items-center space-x-4">
-            <Link to="/">
-              <img
-                src={logo}
-                alt="FAO Logo"
-                className="max-h-24 max-w-xs h-auto w-auto"
-              />
-            </Link>
-          </div>
+    <nav className="bg-green-600 shadow-lg w-full">
+      <div className="container mx-4">
+        <div className="flex justify-between items-center">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="FAO Logo"
+              className="max-h-24 max-w-xs h-auto w-auto"
+            />
+          </Link>
         </div>
       </div>
     </nav>
