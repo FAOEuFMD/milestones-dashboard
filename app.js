2ff5e95f3465a644fd16be4c4348278e177678bc
SHRUDHI PRIYA <152276750+ShrudhiPriya@users.noreply.github.com>
2024-08-27 23:05:43 +0800

    feat(api): add endpoints for target table (#40)
    * refactor(api): update app.js, database.js, .env, and route files based on code review feedback
    
    * feat: set up MySQL connection, query helper function, and configure routes in targets.js
    
    ---------
    
    Co-authored-by: Sophie <sophie@sssoja.com>

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const cors = require("cors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var milestonesRouter = require("./routes/milestones");
+const homeRouter = require("./routes/home");
+const targetsRouter = require("./routes/targets");
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/milestones", milestonesRouter);
+app.use("/api", homeRouter);
+app.use("/api", targetsRouter);
 
 module.exports = app;
