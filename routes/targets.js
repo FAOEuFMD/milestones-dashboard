2684c8675e5908aa74d1d243d753916cc64d24a0
Andrea Cortés <132715367+ACU017@users.noreply.github.com>
2024-09-07 12:49:58 +0200

    Revert "feat: add table component and create join table (#55)"
    This reverts commit 9cae1a67abc7912ca8e0bddb57bcd001405e73f3.

--- routes/targets.js
+++ routes/targets.js
@@ -6,27 +6,6 @@ router.get("/health-check", (req, res) => {
   res.send("Server is running!");
 });
 
-router.get("/alltargets", async (req, res) => {
-  try {
-    const result = await db.query(`
-      SELECT 
-        t.*,
-        fo.name AS focus_objective_name,
-        ka.name AS key_area_name
-      FROM 
-        targets t
-        LEFT JOIN focus_objectives fo ON t.key_areas_id = fo.id
-        LEFT JOIN key_areas ka ON t.key_areas_id = ka.id
-      ORDER BY 
-        t.id;
-    `);
-    res.json(result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error fetching targets" });
-  }
-});
-
 router.get("/targets", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM targets");
@@ -37,12 +16,5 @@ router.get("/targets", async (req, res) => {
     res.status(500).json({ message: "Database query failed" });
   }
 });
-
-
-
-
-
-
-
 
 module.exports = router;
