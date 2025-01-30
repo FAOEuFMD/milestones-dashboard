39aa02963ef604619f7e5bd9802c5cc10c1892d1
Claire Carter <carter.claire.e@gmail.com>
2025-01-30 12:38:03 -0800

    feature: added footer (Closes #81)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,17 +5,15 @@ import logo from "../assets/EuFMD_2023_white_xl.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="nav w-full bg-gradient-to-t from-darkTeal to-darkGreen py-0 h-14 flex items-center">
-      <div className="container ml-4 mt-0 mb-0">
-        <div className="flex justify-between items-center">
+    <nav className="w-full bg-gradient-to-t from-darkTeal to-darkGreen h-16 flex items-center">
+      <div className="ml-4">
           <Link to="/">
             <img
               src={logo}
               alt="FAO Logo"
-              className="max-h-20 max-w-xs h-auto w-auto"
+              className="max-h-28 w-auto"
             />
           </Link>
-        </div>
       </div>
     </nav>
   );
