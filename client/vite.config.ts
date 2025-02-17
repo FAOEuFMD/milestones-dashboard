5d343a719f09ba529d7f8aa9b7c340941bdb4d28
Claire Carter <carter.claire.e@gmail.com>
2025-02-17 10:58:44 -0800

    added dynamic imports to try to reduce plotly chunk size

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Resolve plotly.js/dist/plotly to the basic version
+      'plotly.js/dist/plotly': 'plotly.js-basic-dist',
+    },
+  },
   build: {
     rollupOptions: {
       output: {
@@ -11,6 +17,10 @@ export default defineConfig({
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0]; // chunk by package name
+          }
+          // Specific chunking rule for Plotly to ensure it's in its own chunk
+          if (id.includes('plotly.js-basic-dist')) {
+            return 'plotly'; // Force Plotly into a separate chunk
           }
         }
       }
