f96388ceca601ca497b7f4255f4528b0266a4b1b
Claire Carter <carter.claire.e@gmail.com>
2025-02-19 09:43:26 -0800

    removed dynamic imports of plotly

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     alias: {
       // Resolve plotly.js/dist/plotly to the basic version
       'plotly.js/dist/plotly': 'plotly.js-basic-dist',
+      'plotly.js': 'plotly.js-basic-dist',
     },
   },
   build: {
@@ -17,10 +18,6 @@ export default defineConfig({
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0]; // chunk by package name
-          }
-          // Specific chunking rule for Plotly to ensure it's in its own chunk
-          if (id.includes('plotly.js-basic-dist')) {
-            return 'plotly'; // Force Plotly into a separate chunk
           }
         }
       }
